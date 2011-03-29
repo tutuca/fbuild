2de3e225fd79cd6ad92ef23a23ca9fa1482ce4ba
esteban <none@none>
2011-03-29 01:41:48 -0700

    Solved component dependency at linking level, still have to solve header inclusion paths

--- trunk/buildscripts/component.py
+++ trunk/buildscripts/component.py
@@ -2,9 +2,17 @@
 
 # This file contains wrappers to the creation of the different component types
 # this way we can improve the parameters and unify them
+import os
 
-def CreateProgram(env, name, src):
-    program = env.Program(name, src)
+def CreateProgram(env, name, src, deps):
+    libPaths = []
+    # we have to add all build paths because the build path could
+    # be different to the dependency
+    for root, dirnames, filenames in os.walk(env['BUILD_DIR']):
+        for dirname in dirnames:
+            libPaths.append(os.path.join(root,dirname))
+    print libPaths
+    program = env.Program(name, src, LIBS=deps, LIBPATH=libPaths)
     env.Install(env['INSTALL_DIR'], program)
 
 def CreateStaticLibrary(env, name, src):
