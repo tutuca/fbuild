55f6390e3021bc6a216ff028031655a88814cc17
esteban.papp <none@none>
2011-09-22 03:21:32 -0300

    Merged the addition of the -fPIC flag for AutoTools

--- site_scons/builders.py
+++ site_scons/builders.py
@@ -16,5 +16,11 @@ def configure(target, source, env):
     buildDir = env['buildDir']
     configure = env['configurePath']
     configureOpts = (' --bindir=%(INSTALL_BIN_DIR)s --libdir=%(INSTALL_LIB_DIR)s --includedir=%(INSTALL_HEADERS_DIR)s' % env)
-    return subprocess.call(configure + configureOpts + ' ; make; make install', cwd=buildDir, shell=True)
+    procEnv = os.environ
+    (arch,binType) = platform.architecture()
+    if arch == '64bit':
+        procEnv["CXXFLAGS"] = str(env["CXXFLAGS"])
+        procEnv["CFLAGS"] = '-fPIC'
+
+    return subprocess.call(configure + configureOpts + ' ; make; make install', cwd=buildDir, shell=True, env=procEnv)
 
