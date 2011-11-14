e4d7bbaade174c9d452907f70dca91dfa41b2d38
Hugo Arregui <hugo.arregui@gmail.com>
2011-11-14 18:13:29 -0300

    fixes issue33

--- bin/fbuild.py
+++ bin/fbuild.py
@@ -38,29 +38,47 @@ def parse_project_arg(arg):
     return (arg, splitted[0], splitted[1] if len(splitted) > 1 else None)
 
 parser = argparse.ArgumentParser(description="invokes the fudepan-build system")
-parser.add_argument('-c', dest='commands', help="clear", action='append_const', const='clear')
+parser.add_argument('--type', dest='type', nargs=1, action='store', help='type of build, options: dbg, opt')
+parser.add_argument('--efective', dest='commands', help="adds -Weffc flag", action='append_const', const='efective')
 parser.add_argument('-l', '--list', dest='commands', help="list projects", action='append_const', const='projects')
-parser.add_argument('project', nargs='*', help="use project[:task]. Possibles tasks are: test, " + ", ".join(tasks.keys()))
+parser.add_argument('project', nargs='*', help="use project[:task]. Possibles tasks are: test, clear, clear-test, " + ", ".join(tasks.keys()))
 args = parser.parse_args()
+
+missing_args = True
 
 from dependencies import downloadDependency, findLoadableDependencies
 deps = findLoadableDependencies({}, "conf")
 
 scons_args = []
+
+if args.type:
+    scons_args.append('--type=%s' % args.type[0])
+
 for command in args.commands or []:
-    if command == 'clear':
-        scons_args.append('-c')
-    elif command == 'projects':
+    if command in ['effective']:
+        scons_args.append(command)
+    if command == 'projects':
+        missing_args = False
         print deps.keys()
 
 scons_targets = []
 env = dict(projects=deps)
 for arg in args.project or []:
+    missing_args = False
     original, project, task = parse_project_arg(arg)
     if task in tasks:
         env['original'] = original
         tasks[task](project, task, env)
+    elif task == 'clear':
+        scons_args.append('-c')
+        scons_targets.append(project)
+    elif task == 'clear-test':
+        scons_args.append('-c')
+        scons_targets.append(project + ':test')
     else:
         scons_targets.append(original)
 
 invoke_scons(scons_args + scons_targets)
+
+if missing_args:
+    parser.print_help()
