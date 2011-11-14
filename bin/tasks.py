56dc96e43477334bd3cf4597fde18f1ff81c1bd2
Hugo Arregui <hugo.arregui@gmail.com>
2011-11-14 19:20:31 -0300

    adding gpl heading for tasks.py

--- bin/tasks.py
+++ bin/tasks.py
@@ -1,3 +1,22 @@
+# fudepan-build: The build system for FuDePAN projects 
+#
+# Copyright (C) 2011 Hugo Arregui FuDePAN
+# 
+# This file is part of the fudepan-build build system.
+# 
+# fudepan-build is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# fudepan-build is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with fudepan-build.  If not, see <http://www.gnu.org/licenses/>.
+
 from subprocess import call, check_output
 from termcolor import cprint
 
