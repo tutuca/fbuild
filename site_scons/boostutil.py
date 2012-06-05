83a26098c32655644a6790d0fc52268b5f72416d
esteban.papp <none@none>
2012-06-04 22:31:52 -0700

    Added odeint and boost as projects for project visi

--- site_scons/boostutil.py
+++ site_scons/boostutil.py
@@ -24,7 +24,7 @@ import glob
 def addBoostComponents(env):
     # This is a base component, it will include the qt base include path
     validModules = [
-	'boost_system',
+        'boost_system',
         'boost_thread'
         ]
     for module in validModules:
