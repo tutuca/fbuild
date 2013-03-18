57e3c3464080d935ea13229ba374954fe5706689
GonzaloBonigo <none@none>
2013-03-18 10:23:48 -0300

    fbuild does not work with mercurial version 2.1.
    --HG--
    branch : issue81

--- start.sh
+++ start.sh
@@ -29,10 +29,16 @@ fi
 # Update the fudepan environment
 if [[ "$(which hg)" && -z $FBUILD_NO_UPDATE ]]; then
     if [ $internet_connection = "ok" ]; then
-        hg_version= hg --version | grep "version"|cut -d"(" -f2|cut -d" " -f2|cut -d")" -f1
-        if [$hg_version=="2.1.0" 
-        echo -e "\e[0;35mChecking for updates in the environment\e[0m"
-        hg pull -u
+        # Check mercurial version.
+        #hg_version=$(hg --version | grep "version"|cut -d"(" -f2|cut -d" " -f2|cut -d")" -f1)
+        hg_version=2.1
+        if [[ "$hg_version" == "2.1.0" || "$hg_version" == "2.1" ]]; then
+            # fbuild doesn't work with mercurial 2.1.
+            echo -e "\e[0;31m[error] fbuild does not work with mercurial version 2.1.\e[0m"
+        else
+            echo -e "\e[0;35mChecking for updates in the environment\e[0m"
+            hg pull -u
+        fi
     else
         echo -e "\e[0;33m[warn] FuDePan environment not updated since there is no internet connection\e[0m"
     fi
