d6678b1b816f6d5d3e2375a741efaf1b301e55f5
GonzaloBonigo <none@none>
2013-03-28 15:12:34 -0300

    A change to close the branch.
    --HG--
    branch : issue175
    extra : close : 1

--- site_scons/builders.py
+++ site_scons/builders.py
@@ -1,7 +1,7 @@
 # fudepan-build: The build system for FuDePAN projects
 #
 # Copyright (C) 2011 Esteban Papp, Hugo Arregui, Alejandro Kondrasky,
-#               2013 Gonzalo Bonigo, Gustavo Ojeda, FuDePAN
+#               2013 Gonzalo Bonigo, Gustavo Ojeda, FuDePAN.
 #
 # This file is part of the fudepan-build build system.
 #
