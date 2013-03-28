908073b4c313480c3995b9ed88cdb9633b77bd5d
GonzaloBonigo <none@none>
2013-03-28 19:48:07 -0300

    external_dependencies.xml: pick a default package manager (not working very well).
    --HG--
    branch : issue155

--- site_scons/dependencies.py
+++ site_scons/dependencies.py
@@ -398,6 +398,12 @@ def _CreateExternalDependenciesTargets(env):
                 if distro==DISTRO or distro=='*':
                     componentTarget = installer.getAttribute('target')
                     componentType = installer.getAttribute('manager')
+                    if componentType=="*" and componentType=="LIB":
+                        componentType = MANAGER
+                    else:
+                        env.Cprint('[ERROR] external_dependencies.xml => 
+                        component %s has default manager but is not of type LIB' % 
+                        componentName)
                     break
             dep = _CreateDependency(env,
                                     componentName,
