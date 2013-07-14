4b7523f5c165653442f3d21edc4e856566b2d75f
GonzaloBonigo <none@none>
2013-07-14 13:04:02 -0300

    [issue224] Problem solved.
    --HG--
    branch : issue224

--- site_scons/linker.py
+++ site_scons/linker.py
@@ -37,6 +37,6 @@ def LinuxOptions(env):
     # the install dir. This makes easier to deploy somewhere
     # else, you only need to pass INSTALL_<BIN/LIB/HEADERS>_DIR parameters to
     # scons to deploy it somewhere else.
-    #env.Append(RPATH=env['INSTALL_LIB_DIR'])
-    #env['ENV']['LD_LIBRARY_PATH'] = env['INSTALL_LIB_DIR']
-    pass
+    env.Append(RPATH=env['INSTALL_LIB_DIR'])
+    env['ENV']['LD_LIBRARY_PATH'] = env['INSTALL_LIB_DIR']
+
