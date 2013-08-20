26791ee6a75fa15466822147eb11e9d45c12b8e4
Matias Iturburu <matias.iturburu@tallertechnologies.com>
2013-08-20 16:34:12 -0300

    Reverting missing lcov flag

--- site_scons/builders.py
+++ site_scons/builders.py
@@ -151,6 +151,7 @@ def RunLcov(env, target, source):
         'rm -f %(coverage_file)s' % data,
         'lcov --no-checksum --directory %(project_dir)s -b . --capture --output-file %(coverage_file)s' % data,
         'lcov --no-checksum --directory %(project_dir)s -b . --capture --output-file %(coverage_file)s' % data,
+        'lcov --no-checksum --directory %(project_dir)s -b . --capture --ignore-error source --output-file %(coverage_file)s' % data,
         'lcov --remove %(coverage_file)s "*usr/include*" -o %(coverage_file)s' % data,
         'lcov --remove %(coverage_file)s "*/tests/*" -o %(coverage_file)s' % data
     ]
