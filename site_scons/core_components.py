860451776b0ebe65db4c8ccd3a881a6724d2ac50
leandromoreno <none@none>
2013-10-28 15:40:14 -0300

    [issue263] Created target all:astyle-check.
    --HG--
    branch : issue263

--- site_scons/core_components.py
+++ site_scons/core_components.py
@@ -246,7 +246,7 @@ class Component(object):
     #
     def _SetTargets(self):
         """Create targets for most modules."""
-        self._CreateAstyleCheckTarget(self._sources)
+        run_astyle_check_builder = self._CreateAstyleCheckTarget(self._sources)
         self._CreateAstyleTarget(self._sources)
         run_cccc_builder = self._CreateCCCCTarget(self._sources)
         self._CreateClocTarget(self._sources)
@@ -259,10 +259,11 @@ class Component(object):
         # Create the alias for 'all:info'
         self._env.Alias('all:info', run_info_builder, 'Take info about all projects')
 		# Create the alias for 'all:static-analysis'
-        self._env.Alias('all:static-analysis', run_static_builder, 'Run Static Analisis in all projects')        
+        self._env.Alias('all:static-analysis', run_static_builder, 'Run Static Analisis in all projects')
 		# Create the alias for 'all:doc'
         self._env.Alias('all:doc', run_doc_builder, 'Take the docs of all projects')
-
+        # Create the alias for 'all:astyle-check'
+        self._env.Alias('all:astyle-check', run_astyle_check_builder, 'Run Astyle Check in all projects')
 
     def _GetObjectsFiles(self, object_files, stack):
         """
